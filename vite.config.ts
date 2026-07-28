69d1ac0bfa6ef6fca499f78b47b9ef718e295577
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2026-07-28 08:33:31 +0000

    added polled watch to vite config for rendering instantly changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "/projects/banana/",
+  base: "/memory/",
   server: {
     host: '0.0.0.0',
     port: 5173,
+    watch: {
+      usePolling: true,
+      interval: 100
+    },
   },
 });
