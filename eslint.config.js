615e2f5a95c89205f68e2045cd8f53b2fe150790
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2026-08-05 08:17:02 +0000

    added eslint to project

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+const js = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+
+module.exports = [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module"
+    }
+  }
+];
