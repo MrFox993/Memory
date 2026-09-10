fbe45331c99e7096535677f7eca28ae102943706
Marcel Fuchs <marcel.fuchs.1@gmx.de>
2026-09-10 09:25:26 +0000

    export all central DOM references

---
+++ src/dom/dom-elements.ts
@@ -0,0 +1,52 @@
+export const SHOW_SETTINGS_BUTTON = document.querySelector<HTMLButtonElement>(
+  "#showSettingsButton",
+);
+export const START_BUTTON =
+  document.querySelector<HTMLButtonElement>("#startGameButton");
+export const EXIT_GAME_BUTTON =
+  document.querySelector<HTMLButtonElement>("#exitGameButton");
+export const START_SCREEN = document.querySelector<HTMLElement>("#startScreen");
+export const SETTINGS_SCREEN =
+  document.querySelector<HTMLElement>("#settingsScreen");
+export const GAME_SCREEN = document.querySelector<HTMLElement>("#gameScreen");
+export const GAME_BOARD = document.querySelector<HTMLDivElement>("#gameBoard");
+export const CARD_TEMPLATE = document.querySelector<HTMLTemplateElement>(
+  "#memoryCardTemplate",
+);
+export const ACTIVE_PLAYER_DISPLAY = document.querySelector<HTMLElement>(
+  "#activePlayerDisplay",
+);
+export const ACTIVE_PLAYER_PAWN =
+  document.querySelector<HTMLImageElement>("#activePlayerPawn");
+export const BLUE_SCORE_ELEMENT =
+  document.querySelector<HTMLElement>("#blueScore");
+export const ORANGE_SCORE_ELEMENT =
+  document.querySelector<HTMLElement>("#orangeScore");
+export const BLUE_SCORE_CARD =
+  document.querySelector<HTMLElement>("#blueScoreCard");
+export const ORANGE_SCORE_CARD =
+  document.querySelector<HTMLElement>("#orangeScoreCard");
+export const QUIT_GAME_DIALOG =
+  document.querySelector<HTMLElement>("#quitGameDialog");
+export const BACK_TO_GAME_BUTTON =
+  document.querySelector<HTMLButtonElement>("#backToGameButton");
+export const CONFIRM_QUIT_GAME_BUTTON =
+  document.querySelector<HTMLButtonElement>("#confirmQuitGameButton");
+export const GAME_OVER_SCREEN =
+  document.querySelector<HTMLElement>("#gameOverScreen");
+export const GAME_OVER_PANEL =
+  document.querySelector<HTMLElement>("#gameOverPanel");
+export const WINNER_SCREEN =
+  document.querySelector<HTMLElement>("#winnerScreen");
+export const FINAL_BLUE_SCORE_ELEMENT =
+  document.querySelector<HTMLElement>("#finalBlueScore");
+export const FINAL_ORANGE_SCORE_ELEMENT =
+  document.querySelector<HTMLElement>("#finalOrangeScore");
+export const WINNER_STATUS_ELEMENT =
+  document.querySelector<HTMLElement>("#winnerStatus");
+export const WINNER_IMAGE_ELEMENT =
+  document.querySelector<HTMLImageElement>("#winnerImage");
+export const WINNER_CONFETTI_ELEMENT =
+  document.querySelector<HTMLImageElement>("#winnerConfetti");
+export const BACK_TO_START_BUTTON =
+  document.querySelector<HTMLButtonElement>("#backToStartButton");
